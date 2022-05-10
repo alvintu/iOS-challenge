5367dcfb7df655a342f48025a412d49d39010b80
Alvin Tu <alvintu@alvins-mbp-2.mynetworksettings.com>
2022-05-10 09:51:22 -0400

    Remove extraneous comments and make some string changes

--- Code_Challenge/Resources/LoginViewController.swift
+++ Code_Challenge/Resources/LoginViewController.swift
@@ -16,28 +16,13 @@ class LoginViewController: UIViewController {
 		view.backgroundColor = .green
 		let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
 		button.backgroundColor = .green
-		button.setTitle("Test Button", for: .normal)
+		button.setTitle("Login", for: .normal)
 		button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
 		
 		self.view.addSubview(button)
-
-        // Do any additional setup after loading the view.
     }
 	
 	@objc func buttonAction(sender: UIButton!) {
-		print("Button tapped")
 		self.navigationController?.popViewController(animated: true)
 	}
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
